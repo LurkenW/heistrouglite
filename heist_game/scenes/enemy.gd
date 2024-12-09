84574051c29a3ba4c04a3a7321fa0e1a4048482b
LurkenW <91983594+LurkenW@users.noreply.github.com>
2024-12-09 21:59:06 +0100

    Added basic Enemy
    The enemy only has behaviour to move towards the target

---
+++ heist_game/scenes/enemy.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var target = get_node("/root/root/CharacterBody2D")
+
+const SPEED = 50.0
+
+func _physics_process(delta: float) -> void:
+#	if target != null:
+#		_move_to_target(target)
+	pass
+
+	#Calculates porition of target and movees towards them
+func _move_to_target(target):
+	var direction = global_position.direction_to(target.global_position)
+	velocity = direction * SPEED
+	look_at(target.global_position)
+	move_and_slide()
