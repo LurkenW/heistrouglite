851f4eed45cd2909dda238d6f25a0ac8d7f52dee
LurkenW <91983594+LurkenW@users.noreply.github.com>
2024-12-11 16:19:13 +0100

    Updated Enemy Behaviour
    The enemy is now able to strafe around and will randomly decide how to approach their target.

--- heist_game/scripts/enemy.gd
+++ heist_game/scripts/enemy.gd
@@ -1,21 +1,63 @@
 extends CharacterBody2D
 
-@onready var target = get_node("/root/root/CharacterBody2D")
+@onready var target_player = get_node("/root/root/CharacterBody2D")
 
-const SPEED = 50.0
+var SPEED = 100.0
 var health = 100
+var movement_delay = 1.0
+var movement_timer: Timer
+
+func _ready() -> void:
+	movement_timer = Timer.new()
+	add_child(movement_timer)
+	movement_timer.wait_time = movement_delay
+	movement_timer.timeout.connect(_on_timer_timeout)
+	movement_timer.name = "MovementTimer"
+	movement_timer.start()
 
 func _physics_process(delta: float) -> void:
-#	if target != null:
-#		_move_to_target(target)
-	pass
+	if (self.global_position.distance_to(target_player.global_position) < 100):
+		SPEED = 50
+	else:
+		SPEED = 100
+	look_at(target_player.global_position)
+	move_and_slide()
+
+func _on_timer_timeout():
+	movement_timer.wait_time = movement_delay
+	make_movement_decision(target_player)
+
+func make_movement_decision(target):
+	if (self.global_position.distance_to(target_player.global_position) < 100):
+		movement_delay = 0.5
+		_move_to_target(target)
+	else:
+		var rng = randi_range(0,2)
+		match rng:
+			0:
+				movement_delay = 1.0
+				_move_to_target(target)
+			1:
+				movement_delay = 1.0
+				strafe_target_right(target)
+			2:
+				movement_delay = 1.0
+				strafe_target_left(target)
 
 	#Calculates porition of target and movees towards them
 func _move_to_target(target):
 	var direction = global_position.direction_to(target.global_position)
 	velocity = direction * SPEED
-	look_at(target.global_position)
-	move_and_slide()
+	
+func strafe_target_right(target):
+	var direction = global_position.direction_to(target.global_position)
+	direction = direction.rotated(PI/4)
+	velocity = direction * SPEED
+
+func strafe_target_left(target):
+	var direction = global_position.direction_to(target.global_position)
+	direction = direction.rotated(-PI/4)
+	velocity = direction * SPEED
 
 func takeDamage(incoming_damage):
 	health -= incoming_damage
