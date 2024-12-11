7ccbb80c55bac0c9f66608295b94d1796d3029bb
HH0022 <henrik.hellstrom@bjorkforsen.se>
2024-12-11 15:06:30 +0100

    Updates to the gun
    Created a gun-class
    Created a PlayerGun

--- heist_game/scripts/gun.gd
+++ heist_game/scripts/gun.gd
@@ -1,18 +1,54 @@
-extends Node
+extends Node2D
 class_name Gun
 
-@export var damage: int 
-@export var shootingPoint: Marker2D
-@export var fireCooldown: int
+@export var damage: int = 10
+@export var shootingPoint: Marker2D = null
+@export var fireCooldown: int = 1
 
+var _reloadTimer: Timer
+var _isShooting: bool = false
+var _isReloading: bool = false
 
-func shoot() -> void:
+func _ready() -> void:
+	
+	_checkRequired()
+	
+	_reloadTimer = Timer.new()
+	add_child(_reloadTimer)
+	_reloadTimer.wait_time = fireCooldown
+	_reloadTimer.timeout.connect(_on_timer_timeout)
+	_reloadTimer.name = "ReloadTimer"
+
+func _checkRequired():
+	if shootingPoint == null:
+		push_error("The variable 'Shooting Point' must be set in the Inspector!")
+
+func _shoot() -> void:
+	
+	_reloadTimer.start()
+	_isReloading = true
+	
 	const BULLET = preload("res://scenes/bullet.tscn")
 	var new_bullet = BULLET.instantiate()
 	new_bullet.set_damage(damage)
 	
-	new_bullet.global_position = $WeaponPivot/Icon/ShootingPoint.global_position
-	new_bullet.global_rotation = $WeaponPivot/Icon/ShootingPoint.global_rotation
+	new_bullet.global_position = shootingPoint.global_position
+	new_bullet.global_rotation = shootingPoint.global_rotation
 	
 	get_node("/root").add_child(new_bullet)
-	 
+
+func _on_timer_timeout():
+	
+	if _isShooting:
+		_shoot()
+	else:
+		_isReloading = false
+
+func startShooting() -> void:
+	_isShooting = true
+	
+	if not _isReloading:
+		_shoot()
+
+func stopShooting() -> void:
+	_isShooting = false
